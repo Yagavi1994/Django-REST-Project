ef5897bef1e7f84b77aa73cf189a01d543197600
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-10-16 10:56:16 +0000

    Add default image.

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_qdjgyp'
+        upload_to='images/', default='https://res.cloudinary.com/dz60wxmka/image/upload/v1727864912/default_profile_tqrpbq.jpg'
     )
 
     class Meta:
