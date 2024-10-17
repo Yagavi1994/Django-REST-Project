d123f78a6cfbf69269d1d35dcab59b55af2f4c65
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-10-17 09:59:13 +0000

    Change settings.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -65,7 +65,9 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+# DEBUG = 'DEV' in os.environ
+
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-yagavi1994-djangorestpr-m4eir3o1qdb.ws-eu116.gitpod.io',
